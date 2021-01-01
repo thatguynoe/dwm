7a7bf577dff06562d12070ef2bfdb7d773a79550
thatguynoe <72101983+thatguynoe@users.noreply.github.com>
2021-01-01 15:55:31 -0500

    Added a bind for quickly restarting dwmblocks

--- config.h
+++ config.h
@@ -182,6 +182,7 @@ static Key keys[] = {
     { MODKEY,                       XK_F9,                      spawn,          SHCMD("dmenumount") },
     { MODKEY,                       XK_F10,                     spawn,          SHCMD("dmenuumount") },
     { MODKEY,                       XK_F11,                     spawn,          SHCMD("mpv --no-cache --no-osc --no-input-default-bindings --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") },
+    { MODKEY,                       XK_F12,                     spawn,          SHCMD("killall -q dwmblocks; setsid -f dwmblocks") },
 
     { 0,                            XF86XK_Tools,               spawn,          SHCMD(TERMINAL " -e ncmpcpp") },
     { 0,                            XF86XK_AudioMute,           spawn,          SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") },
