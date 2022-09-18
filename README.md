d0f97eea52374e28f5981c261c67ebcec794a041
thatguynoe <noereyesrivas@gmail.com>
2022-09-17 21:36:33 -0400

    apply the actualfullscreen patch

---
+++ README.md
@@ -0,0 +1,16 @@
+# dwm
+
+My build of dwm.
+
+## Patches and features
+
+* [**actualfullscreen-20211013-cb3f58a**](https://dwm.suckless.org/patches/actualfullscreen/dwm-actualfullscreen-20211013-cb3f58a.diff) provides true fullscreen (<kbd>super+f</kbd>) in dwm.
+    * [This code](https://github.com/LukeSmithxyz/dwm/pull/186/files) makes dwm exit fullscreen if a new window is spawned.
+
+## Installation
+
+```
+git clone https://github.com/thatguynoe/dwm
+cd dwm
+sudo make install
+```
--- dwm.c
+++ dwm.c
@@ -212,6 +212,7 @@ static void tagmon(const Arg *arg);
 static void tile(Monitor *m);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
+static void togglefullscr(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
 static void unfocus(Client *c, int setfocus);
@@ -1071,6 +1072,8 @@ manage(Window w, XWindowAttributes *wa)
 		(unsigned char *) &(c->win), 1);
 	XMoveResizeWindow(dpy, c->win, c->x + 2 * sw, c->y, c->w, c->h); /* some windows require this */
 	setclientstate(c, NormalState);
+	if (selmon->sel && selmon->sel->isfullscreen && !c->isfloating)
+		setfullscreen(selmon->sel, 0);
 	if (c->mon == selmon)
 		unfocus(selmon->sel, 0);
 	c->mon->sel = c;
@@ -1715,6 +1718,13 @@ togglefloating(const Arg *arg)
 		resize(selmon->sel, selmon->sel->x, selmon->sel->y,
 			selmon->sel->w, selmon->sel->h, 0);
 	arrange(selmon);
+}
+
+void
+togglefullscr(const Arg *arg)
+{
+	if (selmon->sel)
+		setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
 }
 
 void
