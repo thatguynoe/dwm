e2e2e983b3990db9974a0f1f1dadcef47e0d06cd
thatguynoe <noereyesrivas@gmail.com>
2025-08-22 18:35:23 -0400

    don't swallow glvis windows

--- config.h
+++ config.h
@@ -59,6 +59,7 @@ static const Rule rules[] = {
     { NULL,      NULL,     "Picture-in-Picture",    0,          1,          0,           0,        -1 },
     { TERMINAL,  NULL,     NULL,                    0,          0,          1,           0,        -1 },
     { NULL,      NULL,     "Event Tester",          0,          0,          0,           1,        -1 }, /* xev */
+    { "glvis",   NULL,     NULL,                    0,          0,          0,           1,        -1 },
     { NULL,      "spterm", NULL,                    SPTAG(0),   1,          1,           0,        -1 },
     { NULL,      "spcalc", NULL,                    SPTAG(1),   1,          1,           0,        -1 },
     { NULL,      "spzoom", NULL,                    SPTAG(2),   1,          1,           0,        -1 },
